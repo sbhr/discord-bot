b187f54633fd9044321a7963fde5fe0ad73743e9
sbhr <test@test.com>
2017-11-14 00:46:01 +0900

    テキストボックスが空欄の時のエラー処理を追加

--- controllers/index.js
+++ controllers/index.js
@@ -9,21 +9,32 @@ const getIndex = async (req, res, next) => {
 
   res.render('index', {
     words,
+    err : {},
+    post: {},
   });
 };
 
 const postWord = async (req, res, next) => {
   const keyword = req.body.keyword;
   const response = req.body.response;
+  const err = {};
+  const post = {};
 
   if (keyword && response) {
     await botDb.insertWord(keyword, response);
+  } else {
+    err. keyword = !keyword;
+    err.response = !response;
+    post.keyword = keyword;
+    post.response = response;
   }
 
   const words = await botDb.getAllWords();
 
   res.render('index', {
     words,
+    err,
+    post,
   });
 };
 
