1c9343233987f5eaf3371b5ad692a2961d360088
sbhr <test@test.com>
2017-11-20 23:19:34 +0900

    更新用ページと更新処理追加

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,10 @@ const index = require('../controllers/index');
 
 /* GET home page. */
 router.get('/', index.getIndex);
+router.get('/detail', index.getDetail);
 
 router.post('/post', index.postWord);
 router.post('/delete', index.deleteWord);
+router.post('/update', index.updateWord);
 
 module.exports = router;
